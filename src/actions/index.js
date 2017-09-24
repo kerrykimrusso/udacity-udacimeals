e35a8fc71de4f0c1df9d265423458a20fd30b26f
Kerry <kerry.kim.russo@gmail.com>
2017-09-23 22:58:50 -0400

    create action types and action creators for ADD_RECIPE and REMOVE_FROM_CALENDAR

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+
+export function addRecipe({day, recipe, meal}) {
+    type: ADD_RECIPE,
+    day,
+    recipe,
+    meal
+}
+
+export function removeFromCalendar({day, meal}) {
+    return {
+        type: REMOVE_FROM_CALENDAR,
+        day,
+        meal
+    }
+}
